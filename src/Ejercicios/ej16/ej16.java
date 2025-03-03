a08590b9780f10190f133d264bfc5ba0b3be0e7c
tony <tonyroses2005@gmail.com>
2025-03-03 20:46:47 +0100

    files

--- src/Ejercicios/ej16/ej16.java
+++ src/Ejercicios/ej16/ej16.java
@@ -1,4 +1,4 @@
 package Ejercicios.ej16;
-
+//16. Programa que afija línies de text codificades a un fitxer de text. El programa ha de permetre afegir una línia de text nova codificada o mostrar tot el contingut del fitxer (codificat o descodificat). Pots inventar-te l'algorisme o forma de codificació.
 public class ej16 {
 }
